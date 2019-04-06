f8fd827e3ae40011f21ce2e3ce826fb06bf7c045
superPershing <727342027@qq.com>
2019-04-06 17:19:26 +0800

    feat(practice4): finish practice4

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,28 @@
 package practice04;
 
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String basicIntroduce() {
+        return String.format("My name is Tom. I am %d years old.", getAge());
+    }
+
+    public String introduce() {
+        return basicIntroduce();
+    }
+
 }
--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,19 @@
 package practice04;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Student. I am at Class %d.", super.basicIntroduce(), getKlass());
+    }
 }
