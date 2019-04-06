fa342cc43edfb6688344b75928ab078de3a0b978
superPershing <727342027@qq.com>
2019-04-06 17:43:24 +0800

    feat(practice6): finish practice6

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,27 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (klass == 0) {
+            return String.format("%s I am a Teacher. I teach No Class.", super.introduce());
+        } else {
+            return String.format("%s I am a Teacher. I teach Class %d.", super.introduce(), getKlass());
+        }
+    }
 }
