8a6dfdecc97d0d40006462bdcb8491bdf71a08ac
superPershing <727342027@qq.com>
2019-04-09 23:51:09 +0800

    feat(practice08): finish practice8

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,31 @@
 package practice08;
 
+import javax.naming.directory.NoSuchAttributeException;
+
 public class Klass {
+    private int number;
+    private Student student;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", getNumber());
+    }
+
+    public Student getLeader() throws NoSuchAttributeException {
+        if (student != null) {
+            return this.student;
+        }
+        throw new NoSuchAttributeException();
+    }
+
+    public void assignLeader(Student student) {
+        this.student = student;
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,28 @@
 package practice08;
 
-public class Student {
+import javax.naming.directory.NoSuchAttributeException;
+
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        try {
+            if (klass.getLeader().equals(this)) {
+                return super.introduce() + " I am a Student. I am Leader of " + klass.getDisplayName() + ".";
+            }
+        } catch (NoSuchAttributeException e) {
+            return super.introduce() + " I am a Student. I am at " + klass.getDisplayName() + ".";
+        }
+        return super.introduce() + " I am a Student. I am at " + klass.getDisplayName() + ".";
+    }
 }
