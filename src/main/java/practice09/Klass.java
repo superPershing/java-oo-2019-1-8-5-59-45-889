11c143b83ed536a96dbaf9cbc8f523d984796e96
superPershing <727342027@qq.com>
2019-04-10 16:28:53 +0800

    feat(practice09): finish practice9

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,36 @@
 package practice09;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private int number;
+    private ArrayList<Student> students = new ArrayList<>();
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass().getNumber() == number) {
+            this.leader = student;
+        }
+        System.out.println("It is not one of us.");
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void appendMember(Student student) {
+        students.add(student);
+    }
 }
